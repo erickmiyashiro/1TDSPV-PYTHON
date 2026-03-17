f66937e02d52488d7e9f11a3703dd79707ef4243
erickmiyashiro <erickmiyashiro@gmail.com>
2026-03-17 15:07:01 -0300

    exercicio 9-8

---
+++ exercicio8.py
@@ -0,0 +1,7 @@
+cal = int(input("Quantas calças voce tem?"))
+cam = int(input("Quantas camisas voce tem?"))
+sap = int(input("Quantos sapatos voce tem?"))
+
+comb = cal * cam * sap
+
+print("Voce tem:", comb,"combinaçoes possiveis")
---
+++ exercico9.py
@@ -0,0 +1,9 @@
+print("O valor de um tenis é R$500,00")
+desc = int(input("Escolha quantos % desconto esse tenis terá: "))
+
+cal = desc / 100 
+cal = 500 * cal
+final = 500 - cal
+
+print("O tenis terá:", cal,"de desconto")
+print("O preço final será:", final,)
